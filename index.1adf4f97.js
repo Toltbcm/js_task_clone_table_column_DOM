e4777c17d97215a1e58a2983c6e38149c7fec0ff
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-18 02:46:17 +0300

    make build

---
+++ index.1adf4f97.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=!0,l=!1,t=void 0;try{for(var n,o=e.rows[Symbol.iterator]();!(r=(n=o.next()).done);r=!0){var a=n.value;a.cells[a.cells.length-1].insertAdjacentHTML("beforebegin",a.cells[1].outerHTML)}}catch(e){l=!0,t=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.1adf4f97.js.map
